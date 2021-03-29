6ec9ae22bb406389a71edb801bd2d27c99e75071
Divyesh Vekariya <170543109018@darshan.ac.in>
2021-03-29 16:27:10 +0530

    configure mosaic layout

--- MosaicLayoutDemo/ViewController.swift
+++ MosaicLayoutDemo/ViewController.swift
@@ -8,12 +8,70 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    lazy var collectionView:UICollectionView = {
+        let view = UICollectionView(frame: .zero, collectionViewLayout: getLayout())
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "cell")
+        view.dataSource = self
+        view.backgroundColor = nil
+        return view
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.addSubview(collectionView)
+        [
+            collectionView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            collectionView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            collectionView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            collectionView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+        ].forEach({ $0.isActive = true })
     }
 
+    private func getLayout() -> UICollectionViewCompositionalLayout {
+        
+        let sItem1 = NSCollectionLayoutItem(layoutSize: .init(widthDimension: .fractionalWidth(0.5), heightDimension: .fractionalHeight(1)))
+        sItem1.contentInsets.trailing = 4
+        let sItem2 = NSCollectionLayoutItem(layoutSize: .init(widthDimension: .fractionalWidth(0.5), heightDimension: .fractionalHeight(1)))
+        sItem2.contentInsets.leading = 4
+        
+        let hGroupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .fractionalHeight(0.5))
+        let hGroup = NSCollectionLayoutGroup.horizontal(layoutSize: hGroupSize, subitems: [sItem1, sItem2])
+        hGroup.contentInsets.top = 4
+        
+        let mItem = NSCollectionLayoutItem(layoutSize: .init(widthDimension: .fractionalWidth(1), heightDimension: .fractionalHeight(0.5)))
+        mItem.contentInsets.bottom = 4
+        
+        let vGroupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(0.5), heightDimension: .fractionalHeight(1))
+        let vGroup = NSCollectionLayoutGroup.vertical(layoutSize: vGroupSize, subitems: [mItem, hGroup])
+        vGroup.contentInsets.leading = 4
+        
+        let lItem = NSCollectionLayoutItem(layoutSize: .init(widthDimension: .fractionalWidth(0.5), heightDimension: .fractionalHeight(1)))
+        lItem.contentInsets.trailing = 4
+        
+        let mainHGroupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .fractionalWidth(1))
+        let mainHGroup = NSCollectionLayoutGroup.horizontal(layoutSize: mainHGroupSize, subitems: [lItem, vGroup])
+        
+        let section = NSCollectionLayoutSection(group: mainHGroup)
+        section.contentInsets = .init(top: 8, leading: 8, bottom: 8, trailing: 8)
+        section.interGroupSpacing = 8
+        
+        return .init(section: section)
+    }
 
+}
+
+extension ViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        9
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+        cell.layer.borderColor = UIColor.red.cgColor
+        cell.layer.borderWidth = 2
+        return cell
+    }
 }
 
