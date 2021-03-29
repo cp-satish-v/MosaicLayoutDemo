0b7b05e4151eb50a605f19ce5a49fa0101511459
Divyesh Vekariya <170543109018@darshan.ac.in>
2021-03-29 15:16:15 +0530

    Initial Commit

---
+++ MosaicLayoutDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MosaicLayoutDemo
+//
+//  Created by Divyesh Vekariya on 29/03/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
